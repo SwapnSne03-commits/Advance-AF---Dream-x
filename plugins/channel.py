238336acfc7932646c4aafe07673fcf10148c3bb
SwapnSne03-commits <somsneha03@gmail.com>
2026-04-04 19:57:21 +0530

    Update channel.py
    No poster available images, potrait to landscape

--- plugins/channel.py
+++ plugins/channel.py
@@ -540,7 +540,12 @@ async def send_movie_update(bot, base_name):
                     url=f"https://t.me/{temp.U_NAME}?start=getfile-{base_name.replace(' ', '-')}"
                 )
             ]])
-            size=(2560, 1440) if LANDSCAPE_POSTER and TMDB_POSTER and movie_doc.get("is_backdrop") and not movie_doc.get("error_tmdb") else (853, 1280)
+            poster_url = get_safe_poster(movie_doc)
+            is_fallback = not bool(movie_doc.get("poster_url"))
+           # size=(2560, 1440) if LANDSCAPE_POSTER and TMDB_POSTER and movie_doc.get("is_backdrop") and not movie_doc.get("error_tmdb") else (853, 1280)
+            size = (2560, 1440) if (LANDSCAPE_POSTER and (movie_doc.get("is_backdrop") or is_fallback) and not movie_doc.get("error_tmdb")) else (853, 1280)
+
+            resized_poster = await fetch_image(poster_url, size)
             if not LINK_PREVIEW:
                 poster_url = get_safe_poster(movie_doc)
                 resized_poster = await fetch_image(poster_url, size)
