597056a06d8b9b45d99f0b5a311dbd719c56df57
SwapnSne03-commits <somsneha03@gmail.com>
2026-04-04 20:16:30 +0530

    Ignore Word More Added ✅

--- plugins/pmfilter.py
+++ plugins/pmfilter.py
@@ -39,7 +39,7 @@ SPELL_CHECK = {}
 temp.IMDB_CAP.clear()
 
 IGNORE_WORDS = [
-    "movies", "movie", "episode", "episodes", "south indian", "south indian movie",
+    "movies", "480p", "720p", "1080p", "session", "movie", "episode", "episodes", "south indian", "south indian movie",
     "south movie", "south indian", "web-series", "web series", "webseries", "hindi me bhejo",
     "ful", "!", "kro", "jaldi", "audio", "language", "mkv", "mp4", "web", "series",
     "hollywood", "all", "bollywood", "south", "hd", "karo", "upload", "bhejo",
